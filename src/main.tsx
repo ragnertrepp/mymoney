96d7d6515a11005bd3c99f268eba3019247ae5b3
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 06:34:58 +0300

    Load MyMoney V4 styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./mobile.css";
 import "./polish.css";
-import "./restore.css";
+import "./v4.css";
 import App from "./AppV4";
 
 createRoot(document.getElementById("root")!).render(
