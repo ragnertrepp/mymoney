c256c6f66d6add317f80644871cdcab5de4d06f8
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 06:57:50 +0300

    Wire planned payments into safe budget display

--- src/AppV4.tsx
+++ src/AppV4.tsx
@@ -1,4 +1,5 @@
 import AppV3 from "./AppV3";
+import SafeBudgetAdjuster from "./SafeBudgetAdjuster";
 import TodayOverview from "./TodayOverview";
 import V5Tools from "./V5Tools";
 
@@ -6,6 +7,7 @@ export default function AppV4() {
   return (
     <>
       <AppV3 />
+      <SafeBudgetAdjuster />
       <TodayOverview />
       <V5Tools />
     </>
