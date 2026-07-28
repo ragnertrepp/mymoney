4c8ff6b8ac94096874348cb033334adff2603371
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 06:49:14 +0300

    Replace floating V4 buttons with unified V5 manager

--- src/AppV4.tsx
+++ src/AppV4.tsx
@@ -1,21 +1,11 @@
 import AppV3 from "./AppV3";
-import BackupRestore from "./BackupRestore";
-import TransactionEditor from "./TransactionEditor";
-import RecurringTransactions from "./RecurringTransactions";
-import PlannedPayments from "./PlannedPayments";
-import MonthlyView from "./MonthlyView";
+import V5Tools from "./V5Tools";
 
 export default function AppV4() {
   return (
     <>
       <AppV3 />
-      <aside className="v4-tools" aria-label="MyMoney V4 tööriistad">
-        <MonthlyView />
-        <PlannedPayments />
-        <RecurringTransactions />
-        <TransactionEditor />
-        <BackupRestore />
-      </aside>
+      <V5Tools />
     </>
   );
 }
