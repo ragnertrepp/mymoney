c5712fb2fd36aa5c5ddcb972ecd263c98adac117
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 06:37:46 +0300

    Add recurring transactions to V4 tools

--- src/AppV4.tsx
+++ src/AppV4.tsx
@@ -1,12 +1,14 @@
 import AppV3 from "./AppV3";
 import BackupRestore from "./BackupRestore";
 import TransactionEditor from "./TransactionEditor";
+import RecurringTransactions from "./RecurringTransactions";
 
 export default function AppV4() {
   return (
     <>
       <AppV3 />
       <aside className="v4-tools" aria-label="MyMoney V4 tööriistad">
+        <RecurringTransactions />
         <TransactionEditor />
         <BackupRestore />
       </aside>
