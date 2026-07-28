8aa5eaa4d84ada231ace682447cf2c6b69e0d3b1
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 06:45:14 +0300

    Add monthly view to V4 tools

--- src/AppV4.tsx
+++ src/AppV4.tsx
@@ -3,12 +3,14 @@ import BackupRestore from "./BackupRestore";
 import TransactionEditor from "./TransactionEditor";
 import RecurringTransactions from "./RecurringTransactions";
 import PlannedPayments from "./PlannedPayments";
+import MonthlyView from "./MonthlyView";
 
 export default function AppV4() {
   return (
     <>
       <AppV3 />
       <aside className="v4-tools" aria-label="MyMoney V4 tööriistad">
+        <MonthlyView />
         <PlannedPayments />
         <RecurringTransactions />
         <TransactionEditor />
