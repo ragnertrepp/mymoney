8eff5645821a148828cb04e53078b003cd71c419
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 18:30:46 +0300

    Add first-run guide and receivables view

--- src/AppV4.tsx
+++ src/AppV4.tsx
@@ -1,10 +1,12 @@
 import AppV3 from "./AppV3";
 import AffordabilityAdjuster from "./AffordabilityAdjuster";
+import DebtReceivablesPortal from "./DebtReceivablesPortal";
 import DeferredIntegrityGuard from "./DeferredIntegrityGuard";
 import SafeBudgetAdjuster from "./SafeBudgetAdjuster";
 import TodayOverview from "./TodayOverview";
 import TopBackupAdjuster from "./TopBackupAdjuster";
 import UndoManagerGuard from "./UndoManager";
+import UserGuide from "./UserGuide";
 import V5Tools from "./V5Tools";
 
 export default function AppV4() {
@@ -12,6 +14,8 @@ export default function AppV4() {
     <>
       <UndoManagerGuard />
       <AppV3 />
+      <UserGuide mode="first-run" />
+      <DebtReceivablesPortal />
       <DeferredIntegrityGuard />
       <SafeBudgetAdjuster />
       <AffordabilityAdjuster />
