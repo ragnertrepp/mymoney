454e00fd1a1dad5f6a557812f6710172cc8be33c
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 06:53:16 +0300

    Show payment alerts on Today view

--- src/AppV4.tsx
+++ src/AppV4.tsx
@@ -1,10 +1,12 @@
 import AppV3 from "./AppV3";
+import TodayOverview from "./TodayOverview";
 import V5Tools from "./V5Tools";
 
 export default function AppV4() {
   return (
     <>
       <AppV3 />
+      <TodayOverview />
       <V5Tools />
     </>
   );
