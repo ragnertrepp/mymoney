1aee9328ab0e601505b255a65902c32ef759250a
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 06:34:31 +0300

    Connect transaction editor to MyMoney V4

--- src/AppV4.tsx
+++ src/AppV4.tsx
@@ -1,11 +1,13 @@
 import AppV3 from "./AppV3";
 import BackupRestore from "./BackupRestore";
+import TransactionEditor from "./TransactionEditor";
 
 export default function AppV4() {
   return (
     <>
       <AppV3 />
-      <aside className="backup-restore-panel" aria-label="Varukoopia taastamine">
+      <aside className="v4-tools" aria-label="MyMoney V4 tööriistad">
+        <TransactionEditor />
         <BackupRestore />
       </aside>
     </>
