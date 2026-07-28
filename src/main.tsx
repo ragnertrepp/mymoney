482239e2ecb605493bcdc7bae09958571fa60362
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 06:08:54 +0300

    Load final UI polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./mobile.css";
+import "./polish.css";
 import App from "./AppV3";
 
 createRoot(document.getElementById("root")!).render(
