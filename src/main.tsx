cb42149989152663457c5b83820c614aaac52660
ragnertrepp <ragner.trepp@gmail.com>
2026-07-28 07:23:05 +0300

    Load month comparison styles

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./v5.css";
 import "./today-overview.css";
 import "./search.css";
 import "./category-summary.css";
+import "./month-comparison.css";
 import App from "./AppV4";
 
 createRoot(document.getElementById("root")!).render(
